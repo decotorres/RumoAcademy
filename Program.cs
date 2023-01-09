2487d2200f7ddb8a96f80fe9a6f5917f21618258
decotorres <121913526+decotorres@users.noreply.github.com>
2023-01-08 21:14:59 -0300

    Exercicio consumoEdistancia

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsumoDeUmVeiculo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int distancia;
+            double consumo;
+
+            Console.WriteLine("Informe a distancia a percorrer:");
+            distancia = int.Parse(Console.ReadLine());
+
+            consumo = distancia / 12.00;
+
+            Console.WriteLine("A quantidade de combuntível é: {0:N}", consumo);
+
+
+
+        }
+    }
+}

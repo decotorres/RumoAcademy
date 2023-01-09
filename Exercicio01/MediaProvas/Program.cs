13cd79fd21ffd7dded0ea75ec8ec2e1b7bff605a
decotorres <121913526+decotorres@users.noreply.github.com>
2023-01-08 21:22:39 -0300

    Organização de pastas entregues primeiro exercicio

---
+++ Exercicio01/MediaProvas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Dynamic;
+
+namespace MediaProvas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nota1, nota2;
+            int media;
+
+            Console.WriteLine("Digite a primeira nota: ");
+            nota1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite a segunda nota: ");
+            nota2 = Convert.ToInt32(Console.ReadLine());
+
+            media = (nota1 + nota2) / 2;
+
+            if (media >= 7)
+            {
+                Console.WriteLine("Aluno Aprovado");
+
+            }else
+            {
+                Console.WriteLine("Aluno Reprovado");
+
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
